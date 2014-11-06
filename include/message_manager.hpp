769bccea9102966564b33c1911fdd908f4b1d2b1
Kacper Kołodziej <kacper@kolodziej.in>
2014-11-06 21:00:26 +0100

    basic implementation of MessageManager

--- include/message_manager.hpp
+++ include/message_manager.hpp
@@ -1,7 +1,12 @@
 #ifndef TAMANDUA_MESSAGE_MANAGER_HPP
 #define TAMANDUA_MESSAGE_MANAGER_HPP
 
+#include "config/message_types.hpp"
+
 #include <deque>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 namespace tamandua {
 	
@@ -12,14 +17,35 @@ class MessageManager
 {
 	private:
 		Server& server_;
-		std::deque<Message> received_messages_;
-		std::deque<Message> messages_to_send_;
+		std::deque<Message> process_queue_;
+		std::thread processing_thread_;
+		std::mutex process_queue_lock_;
+		std::condition_variable process_cv_;
+
+		std::deque<Message> send_queue_;
+		std::thread sending_thread_;
+		std::mutex send_queue_lock_;
+		std::condition_variable send_cv_;
+
+		config::MessageId last_message_id_;
+		bool processing_thread_run_;
+		bool sending_thread_run_;
 
 	public:
 		MessageManager();
+		~MessageManager();
 		MessageManager(const MessageManager&) = delete;
 
 		Server& server();
+		void processMessage(Message&);
+		void sendMessage(Message&);
+
+		void processingThread();
+		void sendingThread();
+
+	private:
+		config::MessageId getNextMessageId_();
+		
 };
 
 } // namespace tamandua
---
+++ src/message_manager.cpp
@@ -0,0 +1,72 @@
+#include "message_manager.hpp"
+
+#include "server.hpp"
+#include "message.hpp"
+
+namespace tamandua {
+
+MessageManager::MessageManager() :
+	processing_thread_(true),
+	sending_thread_(true)
+{}
+
+MessageManager::~MessageManager()
+{
+	processing_thread_ = false;
+	sending_thread_ = false;
+	
+}
+
+Server& MessageManager::server()
+{
+	return server_;
+}
+
+void MessageManager::processMessage(Message& message)
+{
+	process_queue_lock_.lock();
+	process_queue_.push_back(message);
+	process_queue_lock_.unlock();
+
+	process_cv_.notify_one();
+}
+
+void MessageManager::sendMessage(Message& message)
+{
+	send_queue_lock_.lock();
+	send_queue_.push_back(message);
+	send_queue_lock_.unlock();
+
+	send_cv_.notify_one();
+}
+
+void MessageManager::processingThread()
+{
+	while (processing_thread_run_)
+	{
+		// procesing messages
+
+		process_cv_.wait(process_queue_lock_, [this]() {
+			return (process_queue_.empty() == false || processing_thread_run_ == false);
+		});
+	}
+}
+
+void MessageManager::sendingThread()
+{
+	while (sending_thread_run_)
+	{
+		// sending messages
+
+		send_cv_.wait(send_queue_lock_, [this]() {
+			return (send_queue_.empty() == false || sending_thread_run_ == false);
+		});
+	}
+}
+
+config::MessageId MessageManager::getNextMessageId_()
+{
+	return ++last_message_id_;
+}
+
+}
