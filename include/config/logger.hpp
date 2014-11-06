8be1ffefb43f7eef6309791a80336e18d693d810
Kacper Kołodziej <kacper@kolodziej.in>
2014-11-06 23:45:30 +0100

    added Logger macros; not tested yet

--- include/config/logger.hpp
+++ include/config/logger.hpp
@@ -2,6 +2,12 @@
 #define TAMANDUA_CONFIG_LOGGER_HPP
 
 #include "logger/logger.hpp"
-#define Log
+#define Log(name, type, ...) tamandua::Logger::getLogger(name).log(type, __VA_ARGS__)
+#define StdLog(name, ...) tamandua::Logger::getLogger(name).log(tamandua::Logger::StandardLog, __VA_ARGS__)
+#define WarningLog(name, ...) tamandua::Logger::getLogger(name).log(tamandua::Logger::Warning, __VA_ARGS__)
+#define ErrorLog(name, ...) tamandua::Logger::getLogger(name).log(tamandua::Logger::Error, __VA_ARGS__)
+#define DebugBasicLog(name, ...) tamandua::Logger::getLogger(name).log(tamandua::Logger::DebugBasic, __VA_ARGS__)
+#define DebugAdvancedLog(name, ...) tamandua::Logger::getLogger(name).log(tamandua::Logger::DebugAdvanced, __VA_ARGS__)
+#define DebugExpertLog(name, ...) tamandua::Logger::getLogger(name).log(tamandua::Logger::DebugExpert, __VA_ARGS__)
 
 #endif
