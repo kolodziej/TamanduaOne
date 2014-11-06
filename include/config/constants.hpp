dffc425bfa2d38724913a3919ac49805f704c53c
Kacper Kołodziej <kacper@kolodziej.in>
2014-11-06 22:19:22 +0100

    added constants config file

---
+++ include/config/constants.hpp
@@ -0,0 +1,10 @@
+#ifndef TAMANDUA_CONFIG_CONSTANTS_HPP
+#define TAMANDUA_CONFIG_CONSTANTS_HPP
+
+namespace tamandua { namespace config {
+
+extern const unsigned int participant_name_size;
+
+} }
+
+#endif
---
+++ src/config/constants.cpp
@@ -0,0 +1,7 @@
+#include "config/constants.hpp"
+
+namespace tamandua { namespace config {
+
+const unsigned int participant_name_size = 40;
+
+} }
