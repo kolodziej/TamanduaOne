a05249e5b0a39f747d798cff4fd3d20f0b2180a8
Kacper Kołodziej <kacper@kolodziej.in>
2014-11-12 17:28:38 +0100

    added declaration of SessionManager

--- include/session_manager.hpp
+++ include/session_manager.hpp
@@ -2,12 +2,37 @@
 #define TAMANDUA_SESSION_MANAGER_HPP
 
 #include "config/tamandua_api.hpp"
+#include "config/session.hpp"
+
+#include <boost/asio.hpp>
 
 namespace tamandua {
 
+class Session;
+
 class TAMANDUA_API SessionManager
 {
+	private:
+		Server& server_;
+		std::map<config::SessionId, std::shared_ptr<Session>> sessions_;
+		boost::asio::ip::tcp::acceptor acceptor_;
+		unsigned int max_session_num_;
+		config::SessionId last_session_id_;
+		bool accepting_;
 
+	public:
+		SessionManager(Server&);
+		SessionManager(const SessionManager&) = delete;
+
+		bool closeSession(config::SessionId);
+		unsigned int maxSessionNum();
+		void setMaxSessionNum(unsigned int);
+		void startAccepting();
+		void stopAccepting();
+		bool accepting();
+
+	private:
+		config::SessionId nextSessionId_();
 };
 
 }
